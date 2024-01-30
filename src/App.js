cacd97c88f93f585170e9ebaa45b4f552efc74f9
rafia_rasheed <rafiarasheed64@gmail.com>
2024-01-31 00:46:07 +0500

    uploaded

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./Component/header"
+import Navbar from "./Component/navbar"
+import First from "./Component/first";
+import Second from "./Component/second";
+import Third from "./Component/third";
+import Fourth from "./Component/fourth";
+import Fifth from "./Component/fifth";
+import Sixth from "./Component/sixth";
+import Footer from "./Component/footer";
+
+
+
+
+
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Navbar/>
+      <First/>
+      <Second/>
+      <Third/>
+      <Fourth/>
+      <Fifth/>
+      <Sixth/>
+      <Footer/>
+    </div>
+  )
+}
+export default App
